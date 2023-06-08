7f5823d0d4510364c5ee70deba4a62f1672c28a8
glenn2030 <122248619+glenn2030@users.noreply.github.com>
2023-06-08 22:00:02 +0800

    Update home.dart

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,6 +1,5 @@
 import 'package:connectcard/models/Cards.dart';
 import 'package:connectcard/models/theUser.dart';
-import 'package:connectcard/screens/authenticate/sign_in.dart';
 import 'package:connectcard/screens/home/cards_form.dart';
 import 'package:connectcard/services/auth.dart';
 import 'package:connectcard/services/database.dart';
@@ -26,13 +25,13 @@ class Home extends StatelessWidget {
       );
     }
 
-    void _signOut(BuildContext context) async {
+    /*void _signOut(BuildContext context) async {
       await _auth.signOut();
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (context) => SignIn(toggleView: null)),
       );
-    }
+    }*/
 
     return StreamBuilder<UserData>(
       stream: DatabaseService(uid: Provider.of<TheUser?>(context)!.uid)
@@ -54,7 +53,7 @@ class Home extends StatelessWidget {
                     icon: const Icon(Icons.person),
                     label: const Text('Logout'),
                     onPressed: () {
-                      _signOut(context);
+                      // _signOut(context);
                     },
                   ),
                   TextButton.icon(
