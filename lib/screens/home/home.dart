391183a04444b1e23ba6cd29f445ade7dd84bf20
glenn2030 <122248619+glenn2030@users.noreply.github.com>
2023-06-08 21:02:22 +0800

    Comment out SignOut

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,10 +1,7 @@
 import 'package:connectcard/models/Cards.dart';
-import 'package:connectcard/models/theUser.dart';
-import 'package:connectcard/screens/authenticate/sign_in.dart';
 import 'package:connectcard/screens/home/cards_form.dart';
 import 'package:connectcard/services/auth.dart';
 import 'package:connectcard/services/database.dart';
-import 'package:connectcard/shared/loading.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -26,60 +23,50 @@ class Home extends StatelessWidget {
       );
     }
 
-    void _signOut(BuildContext context) async {
+    /*void _signOut(BuildContext context) async {
       await _auth.signOut();
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (context) => SignIn(toggleView: null)),
       );
-    }
+    }*/
 
-    return StreamBuilder<UserData>(
-      stream: DatabaseService(uid: Provider.of<TheUser?>(context)!.uid)
-          .userProfile, // Assuming you have a userData stream in your DatabaseService
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          UserData userData = snapshot.data!;
-          return StreamProvider<List<Cards>>.value(
-            value: DatabaseService(uid: userData.uid).cardList,
-            initialData: [],
-            child: Scaffold(
-              backgroundColor: Colors.yellow[800],
-              appBar: AppBar(
-                title: Text(userData.name),
-                backgroundColor: Colors.yellow[800],
-                elevation: 0.0,
-                actions: <Widget>[
-                  TextButton.icon(
-                    icon: const Icon(Icons.person),
-                    label: const Text('Logout'),
-                    onPressed: () {
-                      _signOut(context);
-                    },
-                  ),
-                  TextButton.icon(
-                    icon: const Icon(Icons.edit),
-                    label: const Text('Edit'),
-                    onPressed: () => _showCardsPanel(),
-                  )
-                ],
-              ),
-              //body: CardList(),
-              body: const ListTile(
-                leading: CircleAvatar(
-                  radius: 25.0,
-                  backgroundColor: Colors.white,
-                  child: Icon(Icons.add_a_photo, color: Colors.black),
-                ),
-                title: Text("First Card: Insert name"),
-                subtitle: Text("Insert Job Title"),
-              ),
+    return StreamProvider<List<Cards>>.value(
+      //change this to get uid
+      value: DatabaseService(uid: '123').cardList,
+      initialData: [],
+      child: Scaffold(
+        backgroundColor: Colors.yellow[800],
+        appBar: AppBar(
+          title: const Text('UserName'),
+          backgroundColor: Colors.yellow[800],
+          elevation: 0.0,
+          actions: <Widget>[
+            TextButton.icon(
+              icon: const Icon(Icons.person),
+              label: const Text('Logout'),
+              onPressed: () {
+                //_signOut(context);
+              },
             ),
-          );
-        } else {
-          return Loading();
-        }
-      },
+            TextButton.icon(
+              icon: const Icon(Icons.edit),
+              label: const Text('Edit'),
+              onPressed: () => _showCardsPanel(),
+            )
+          ],
+        ),
+        //body: CardList(),
+        body: const ListTile(
+          leading: CircleAvatar(
+            radius: 25.0,
+            backgroundColor: Colors.white,
+            child: Icon(Icons.add_a_photo, color: Colors.black),
+          ),
+          title: Text("First Card: Insert name"),
+          subtitle: Text("Insert Job Title"),
+        ),
+      ),
     );
   }
 }
