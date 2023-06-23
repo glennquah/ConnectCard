6d3295c4a49cfc7271f2ae45722bb7d09daa08fa
glenn2030 <122248619+glenn2030@users.noreply.github.com>
2023-06-23 18:07:52 +0800

    List view seperated by lines + spacing

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -177,41 +177,61 @@ class Home extends StatelessWidget {
                 Expanded(
                   child: ListView.builder(
                     itemCount: cards.length,
-                    itemExtent: 60,
                     itemBuilder: (context, index) {
                       Cards card = cards[index];
-                      return ListTile(
-                        leading: CircleAvatar(
-                          radius: 40.0,
-                          backgroundColor: Colors.white,
-                          child: card.imageUrl.isNotEmpty
-                              ? ClipOval(
-                                  child: Image.network(
-                                    card.imageUrl,
-                                    width: 60.0,
-                                    height: 60.0,
-                                    fit: BoxFit.cover,
-                                  ),
-                                )
-                              : Icon(
-                                  Icons.add,
-                                  size: 40.0,
-                                  color: Colors.black,
-                                ),
+                      return Container(
+                        decoration: BoxDecoration(
+                          border: Border(
+                            bottom: BorderSide(color: Colors.black),
+                          ),
                         ),
-                        title: Column(
+                        padding: EdgeInsets.only(
+                            bottom: 8.0), // Add spacing between cards
+                        child: ListTile(
+                          contentPadding: EdgeInsets
+                              .zero, // Remove default ListTile padding
+                          leading: CircleAvatar(
+                            radius: 40.0,
+                            backgroundColor: Colors.white,
+                            child: card.imageUrl.isNotEmpty
+                                ? ClipOval(
+                                    child: Image.network(
+                                      card.imageUrl,
+                                      width: 60.0,
+                                      height: 60.0,
+                                      fit: BoxFit.cover,
+                                    ),
+                                  )
+                                : Icon(
+                                    Icons.add,
+                                    size: 40.0,
+                                    color: Colors.black,
+                                  ),
+                          ),
+                          title: Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
                               Text(userData.name),
-                              Text(card.companyName),
-                              Text(card.jobTitle),
-                            ]),
-                        subtitle: Column(
+                              Text(card.companyName.isNotEmpty
+                                  ? card.companyName
+                                  : "Insert Company Name"),
+                              Text(card.jobTitle.isNotEmpty
+                                  ? card.jobTitle
+                                  : "Insert Job Title"),
+                            ],
+                          ),
+                          subtitle: Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              Text(card.phoneNum),
-                              Text(card.email),
-                            ]),
+                              Text(card.phoneNum.isNotEmpty
+                                  ? card.phoneNum
+                                  : "Insert Phone Number"),
+                              Text(card.email.isNotEmpty
+                                  ? card.email
+                                  : "Insert Email"),
+                            ],
+                          ),
+                        ),
                       );
                     },
                   ),
