83043f6e1286f5cf62671369225d552193b089c9
glenn2030 <122248619+glenn2030@users.noreply.github.com>
2023-06-28 16:22:20 +0800

    added text to open url (not done)

--- lib/shared/carouselsliderwidget.dart
+++ lib/shared/carouselsliderwidget.dart
@@ -22,7 +22,7 @@ class _CarouselSliderWidgetState extends State<CarouselSliderWidget> {
     return Column(
       children: [
         SizedBox(
-          height: 20,
+          height: 10,
         ),
         Text(
           widget.cards[_currentIndex]
@@ -33,7 +33,7 @@ class _CarouselSliderWidgetState extends State<CarouselSliderWidget> {
           ),
         ),
         SizedBox(
-          height: 20,
+          height: 10,
         ),
         Flexible(
           child: CarouselSlider.builder(
@@ -41,7 +41,11 @@ class _CarouselSliderWidgetState extends State<CarouselSliderWidget> {
             itemBuilder: (context, index, _) {
               Cards card = widget.cards[index];
               return Container(
-                // ... existing code for the card item
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                margin: EdgeInsets.symmetric(horizontal: 10.0),
                 child: ClipRRect(
                   borderRadius: BorderRadius.circular(10.0),
                   child: ListTile(
@@ -49,26 +53,119 @@ class _CarouselSliderWidgetState extends State<CarouselSliderWidget> {
                     title: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        // ... existing code for other card details
+                        Row(
+                          children: [
+                            CircleAvatar(
+                              radius: 30.0,
+                              backgroundColor: Colors.white,
+                              child: card.imageUrl.isNotEmpty
+                                  ? ClipOval(
+                                      child: Image.network(
+                                        card.imageUrl,
+                                        width: 60.0,
+                                        height: 60.0,
+                                        fit: BoxFit.cover,
+                                      ),
+                                    )
+                                  : Icon(
+                                      Icons.add,
+                                      size: 40.0,
+                                      color: Colors.black,
+                                    ),
+                            ),
+                            SizedBox(width: 10.0),
+                            Align(
+                              alignment: Alignment.topLeft,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    widget.userData.name,
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(
+                                    card.companyName.isNotEmpty
+                                        ? card.companyName
+                                        : "Insert Company Name",
+                                    style: TextStyle(color: Colors.grey[700]),
+                                  ),
+                                  Text(
+                                    card.jobTitle.isNotEmpty
+                                        ? card.jobTitle
+                                        : "Insert Job Title",
+                                    style: TextStyle(color: Colors.grey[700]),
+                                  ),
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                        SizedBox(height: 10.0),
                         GestureDetector(
                           onTap: () {
-                            if (card.phoneNum.isNotEmpty) {
-                              launch('tel:${card.phoneNum}');
-                            }
+                            // Handle phone number click
+                            // Add your logic here
                           },
-                          child: Text(
-                            card.phoneNum.isNotEmpty
-                                ? card.phoneNum
-                                : "Insert Phone Number",
-                            style: TextStyle(
-                              color: Colors.grey[700],
-                              decoration: card.phoneNum.isNotEmpty
-                                  ? TextDecoration.underline
-                                  : TextDecoration.none,
+                          child: Container(
+                            padding: EdgeInsets.symmetric(horizontal: 4.0),
+                            child: Text(
+                              card.phoneNum.isNotEmpty
+                                  ? card.phoneNum
+                                  : "Insert Phone Number",
+                              style: TextStyle(
+                                color: Colors.blue,
+                                decoration: card.phoneNum.isNotEmpty
+                                    ? TextDecoration.underline
+                                    : TextDecoration.none,
+                              ),
                             ),
                           ),
                         ),
-                        // ... existing code for other card details
+                        GestureDetector(
+                          onTap: () {
+                            // Handle email click
+                            // Add your logic here
+                          },
+                          child: Container(
+                            padding: EdgeInsets.symmetric(horizontal: 4.0),
+                            child: Text(
+                              card.email.isNotEmpty
+                                  ? card.email
+                                  : "Insert Email",
+                              style: TextStyle(
+                                color: Colors.blue,
+                                decoration: card.phoneNum.isNotEmpty
+                                    ? TextDecoration.underline
+                                    : TextDecoration.none,
+                              ),
+                            ),
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () async {
+                            final Uri _url = Uri.parse('https://flutter.dev');
+                            if (await canLaunchUrl(_url)) {
+                              await launchUrl(_url);
+                            } else {
+                              print("can't launch this url");
+                            }
+                          },
+                          child: Container(
+                            padding: EdgeInsets.symmetric(horizontal: 4.0),
+                            child: Text(
+                              card.companyWebsite.isNotEmpty
+                                  ? card.companyWebsite
+                                  : "Insert Company Website",
+                              style: TextStyle(
+                                color: Colors.blue,
+                                decoration: card.phoneNum.isNotEmpty
+                                    ? TextDecoration.underline
+                                    : TextDecoration.none,
+                              ),
+                            ),
+                          ),
+                        ),
                       ],
                     ),
                   ),
@@ -76,7 +173,17 @@ class _CarouselSliderWidgetState extends State<CarouselSliderWidget> {
               );
             },
             options: CarouselOptions(
-              // ... existing carousel options
+              height: 200.0,
+              enableInfiniteScroll: false,
+              viewportFraction: 0.8,
+              enlargeCenterPage: true,
+              aspectRatio: 16 / 9,
+              initialPage: 0,
+              autoPlay: false,
+              autoPlayInterval: Duration(seconds: 3),
+              autoPlayAnimationDuration: Duration(milliseconds: 800),
+              autoPlayCurve: Curves.fastOutSlowIn,
+              enlargeStrategy: CenterPageEnlargeStrategy.scale,
               onPageChanged: (index, _) {
                 setState(() {
                   _currentIndex = index;
@@ -85,14 +192,22 @@ class _CarouselSliderWidgetState extends State<CarouselSliderWidget> {
             ),
           ),
         ),
-        SizedBox(height: 20.0),
+        SizedBox(height: 10.0),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: List.generate(
             widget.cards.length,
             (index) => Container(
-                // ... existing code for the indicator dots
-                ),
+              width: 8.0,
+              height: 8.0,
+              margin: EdgeInsets.symmetric(horizontal: 4.0),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: _currentIndex == index
+                    ? Colors.grey[900]
+                    : Colors.grey[400],
+              ),
+            ),
           ),
         ),
       ],
