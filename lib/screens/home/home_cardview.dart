66f428786afcfafc7f340626f35a2b937dbc2c17
glenn2030 <122248619+glenn2030@users.noreply.github.com>
2023-06-28 14:00:50 +0800

    add in cardview
    now able to see cards

--- lib/screens/home/home_cardview.dart
+++ lib/screens/home/home_cardview.dart
@@ -3,77 +3,141 @@ import 'package:connectcard/models/Cards.dart';
 import 'package:connectcard/models/theUser.dart';
 import 'package:flutter/material.dart';
 
-class HomeCardView extends StatelessWidget {
+class HomeCardView extends StatefulWidget {
   final UserData userData;
   final List<Cards> cards;
 
   HomeCardView({required this.userData, required this.cards});
 
   @override
+  _HomeCardViewState createState() => _HomeCardViewState();
+}
+
+class _HomeCardViewState extends State<HomeCardView> {
+  int _currentIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Expanded(
-          child: CarouselSlider.builder(
-            itemCount: cards.length,
-            itemBuilder: (context, index, _) {
-              Cards card = cards[index];
-              return Container(
-                decoration: BoxDecoration(
-                  border: Border(
-                    bottom: BorderSide(color: Colors.black),
-                  ),
-                ),
-                padding: EdgeInsets.only(bottom: 8.0),
+        SizedBox(
+          height: 20,
+        ),
+        CarouselSlider.builder(
+          itemCount: widget.cards.length,
+          itemBuilder: (context, index, _) {
+            Cards card = widget.cards[index];
+            return Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              margin: EdgeInsets.symmetric(horizontal: 10.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10.0),
                 child: ListTile(
-                  contentPadding: EdgeInsets.zero,
-                  leading: CircleAvatar(
-                    radius: 40.0,
-                    backgroundColor: Colors.white,
-                    child: card.imageUrl.isNotEmpty
-                        ? ClipOval(
-                            child: Image.network(
-                              card.imageUrl,
-                              width: 60.0,
-                              height: 60.0,
-                              fit: BoxFit.cover,
-                            ),
-                          )
-                        : Icon(
-                            Icons.add,
-                            size: 40.0,
-                            color: Colors.black,
-                          ),
-                  ),
+                  contentPadding: EdgeInsets.all(8.0),
                   title: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(userData.name),
-                      Text(card.companyName.isNotEmpty
-                          ? card.companyName
-                          : "Insert Company Name"),
-                      Text(card.jobTitle.isNotEmpty
-                          ? card.jobTitle
-                          : "Insert Job Title"),
-                    ],
-                  ),
-                  subtitle: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(card.phoneNum.isNotEmpty
-                          ? card.phoneNum
-                          : "Insert Phone Number"),
-                      Text(card.email.isNotEmpty ? card.email : "Insert Email"),
+                      Row(
+                        children: [
+                          CircleAvatar(
+                            radius: 30.0,
+                            backgroundColor: Colors.white,
+                            child: card.imageUrl.isNotEmpty
+                                ? ClipOval(
+                                    child: Image.network(
+                                      card.imageUrl,
+                                      width: 60.0,
+                                      height: 60.0,
+                                      fit: BoxFit.cover,
+                                    ),
+                                  )
+                                : Icon(
+                                    Icons.add,
+                                    size: 40.0,
+                                    color: Colors.black,
+                                  ),
+                          ),
+                          SizedBox(width: 10.0),
+                          Align(
+                            alignment: Alignment.topLeft,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  widget.userData.name,
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  card.companyName.isNotEmpty
+                                      ? card.companyName
+                                      : "Insert Company Name",
+                                  style: TextStyle(color: Colors.grey[700]),
+                                ),
+                                Text(
+                                  card.jobTitle.isNotEmpty
+                                      ? card.jobTitle
+                                      : "Insert Job Title",
+                                  style: TextStyle(color: Colors.grey[700]),
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                      SizedBox(height: 10.0),
+                      Text(
+                        card.phoneNum.isNotEmpty
+                            ? card.phoneNum
+                            : "Insert Phone Number",
+                        style: TextStyle(color: Colors.grey[700]),
+                      ),
+                      Text(
+                        card.email.isNotEmpty ? card.email : "Insert Email",
+                        style: TextStyle(color: Colors.grey[700]),
+                      ),
                     ],
                   ),
                 ),
-              );
+              ),
+            );
+          },
+          options: CarouselOptions(
+            height: 200.0,
+            enableInfiniteScroll: false,
+            viewportFraction: 0.8,
+            enlargeCenterPage: true,
+            aspectRatio: 16 / 9,
+            initialPage: 0,
+            autoPlay: false,
+            autoPlayInterval: Duration(seconds: 3),
+            autoPlayAnimationDuration: Duration(milliseconds: 800),
+            autoPlayCurve: Curves.fastOutSlowIn,
+            enlargeStrategy: CenterPageEnlargeStrategy.scale,
+            onPageChanged: (index, _) {
+              setState(() {
+                _currentIndex = index;
+              });
             },
-            options: CarouselOptions(
-              height: 400.0,
-              enableInfiniteScroll: false,
-              viewportFraction: 0.8,
-              enlargeCenterPage: true,
+          ),
+        ),
+        SizedBox(height: 20.0),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: List.generate(
+            widget.cards.length,
+            (index) => Container(
+              width: 8.0,
+              height: 8.0,
+              margin: EdgeInsets.symmetric(horizontal: 4.0),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: _currentIndex == index
+                    ? Colors.grey[900]
+                    : Colors.grey[400],
+              ),
             ),
           ),
         ),
