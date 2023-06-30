617e69a18286a19e43f5ecd244ba77f624975234
glenn2030 <122248619+glenn2030@users.noreply.github.com>
2023-06-30 11:27:08 +0800

    Update TheUser.dart

--- lib/models/TheUser.dart
+++ lib/models/TheUser.dart
@@ -13,12 +13,12 @@ class TheUser {
 class UserData {
   final String uid;
   final String name;
-  // final String headLine;
+  final String headLine;
   final List<Cards> listOfCards;
 
   UserData(
       {required this.uid,
-      //     required this.headLine,
+      required this.headLine,
       required this.name,
       required this.listOfCards});
 }
