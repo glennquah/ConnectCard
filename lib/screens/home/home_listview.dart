73fd29df76fe185600b4bc6071971e844f905aa0
glenn2030 <122248619+glenn2030@users.noreply.github.com>
2023-06-30 16:52:57 +0800

    Update home_listview.dart
    touch opacity on the list view to bring you to the details page

--- lib/screens/home/home_listview.dart
+++ lib/screens/home/home_listview.dart
@@ -1,5 +1,10 @@
 import 'package:connectcard/models/Cards.dart';
+<<<<<<< Updated upstream
 import 'package:connectcard/models/theUser.dart';
+=======
+import 'package:connectcard/models/TheUser.dart';
+import 'package:connectcard/shared/card_details.dart';
+>>>>>>> Stashed changes
 import 'package:flutter/material.dart';
 
 class HomeListView extends StatelessWidget {
@@ -17,53 +22,68 @@ class HomeListView extends StatelessWidget {
             itemCount: cards.length,
             itemBuilder: (context, index) {
               Cards card = cards[index];
-              return Container(
-                decoration: BoxDecoration(
-                  border: Border(
-                    bottom: BorderSide(color: Colors.black),
+              return GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => CardDetailsPage(
+                        card: card,
+                        userData: userData,
+                      ),
+                    ),
+                  );
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(color: Colors.black),
+                    ),
                   ),
-                ),
-                padding: EdgeInsets.only(bottom: 8.0),
-                child: ListTile(
-                  contentPadding: EdgeInsets.zero,
-                  leading: CircleAvatar(
-                    radius: 40.0,
-                    backgroundColor: Colors.white,
-                    child: card.imageUrl.isNotEmpty
-                        ? ClipOval(
-                            child: Image.network(
-                              card.imageUrl,
-                              width: 60.0,
-                              height: 60.0,
-                              fit: BoxFit.cover,
+                  padding: EdgeInsets.only(bottom: 8.0),
+                  child: ListTile(
+                    contentPadding: EdgeInsets.zero,
+                    leading: CircleAvatar(
+                      radius: 40.0,
+                      backgroundColor: Colors.white,
+                      child: card.imageUrl.isNotEmpty
+                          ? ClipOval(
+                              child: Image.network(
+                                card.imageUrl,
+                                width: 60.0,
+                                height: 60.0,
+                                fit: BoxFit.cover,
+                              ),
+                            )
+                          : Icon(
+                              Icons.add,
+                              size: 40.0,
+                              color: Colors.black,
                             ),
-                          )
-                        : Icon(
-                            Icons.add,
-                            size: 40.0,
-                            color: Colors.black,
-                          ),
-                  ),
-                  title: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(userData.name),
-                      Text(card.companyName.isNotEmpty
-                          ? card.companyName
-                          : "Insert Company Name"),
-                      Text(card.jobTitle.isNotEmpty
-                          ? card.jobTitle
-                          : "Insert Job Title"),
-                    ],
-                  ),
-                  subtitle: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(card.phoneNum.isNotEmpty
-                          ? card.phoneNum
-                          : "Insert Phone Number"),
-                      Text(card.email.isNotEmpty ? card.email : "Insert Email"),
-                    ],
+                    ),
+                    title: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(userData.name),
+                        Text(card.companyName.isNotEmpty
+                            ? card.companyName
+                            : "Insert Company Name"),
+                        Text(card.jobTitle.isNotEmpty
+                            ? card.jobTitle
+                            : "Insert Job Title"),
+                      ],
+                    ),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(card.phoneNum.isNotEmpty
+                            ? card.phoneNum
+                            : "Insert Phone Number"),
+                        Text(card.email.isNotEmpty
+                            ? card.email
+                            : "Insert Email"),
+                      ],
+                    ),
                   ),
                 ),
               );
