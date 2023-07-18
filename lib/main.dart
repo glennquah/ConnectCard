e198905487922852f87662584a0e54033e3ebf22
Zhi Wei <tanzhiwei7@gmail.com>
2023-07-19 03:27:30 +0800

    NFC test out

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'package:connectcard/services/auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:connectcard/screens/nfc/nfc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
