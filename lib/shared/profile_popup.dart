b1c99ce3b8240dc0dfc9f637b5cdaf65423d7e1d
glenn2030 <quah.glenn@gmail.com>
2023-07-22 15:20:39 +0800

    pop up after adding friend

--- lib/shared/profile_popup.dart
+++ lib/shared/profile_popup.dart
@@ -49,11 +49,51 @@ class ProfilePopup extends StatelessWidget {
                 onPressed: () {
                   onAddFriend();
                   Navigator.pop(context);
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text('Friend request sent'),
-                      duration: Duration(seconds: 2),
-                    ),
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        content: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text('Friend request sent to ${user.name}.'),
+                            SizedBox(height: 16.0),
+                            CircleAvatar(
+                              radius: 30.0,
+                              backgroundImage: NetworkImage(user.profilePic),
+                              backgroundColor: Colors.grey,
+                              child: user.profilePic.isNotEmpty
+                                  ? null
+                                  : Icon(
+                                      Icons.person,
+                                      size: 30.0,
+                                      color: Colors.white,
+                                    ),
+                            ),
+                            SizedBox(height: 16.0),
+                            Text(
+                              '${user.name} #${user.uid.substring(user.uid.length - 4)}',
+                              style: TextStyle(
+                                  fontSize: 18.0, fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(height: 8.0),
+                            Text(
+                              user.headLine,
+                              style:
+                                  TextStyle(fontSize: 14.0, color: Colors.grey),
+                            ),
+                          ],
+                        ),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: Text('Close'),
+                          ),
+                        ],
+                      );
+                    },
                   );
                 },
                 child: Text('Add Friend'),
