d9562b36db912aaf5e3e4c936714a7fe8d673362
Zhi Wei <tanzhiwei7@gmail.com>
2023-07-22 20:31:36 +0800

    Fix Home page after delete card

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -130,7 +130,7 @@ class _HomeState extends State<Home> {
                     globalKeyThree,
                     globalKeyFour
                   ]),
-                  child: const Padding(
+                  child: Padding(
                       padding: EdgeInsets.symmetric(horizontal: 8.0),
                       child: Row(
                         children: [
